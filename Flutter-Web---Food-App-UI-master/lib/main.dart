9c850c9e322cb3a5697f07a6da29fb50e5807ea0
Caio <caioalves682@gmail.com>
2021-06-07 18:06:04 -0300

    enviando arquivo

---
+++ Flutter-Web---Food-App-UI-master/lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:food_web/Screens/Home/home_screen.dart';
+import 'package:food_web/constant.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Hefestus Comunicacao',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primaryColor: kPrimaryColor,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: HomeScreen(),
+    );
+  }
+}
